0fa69de0bc85906de7fd61c4c1601b6a5f623546
Le Hung Quang Minh <lehungquangminh@users.noreply.github.com>
2026-03-04 06:10:15 +0700

    chore(release): bump thagc version to 1.8.0

--- compiler/include/thagc/shared/version.hpp
+++ compiler/include/thagc/shared/version.hpp
@@ -4,7 +4,7 @@
 
 namespace thagc::support {
 
-inline constexpr std::string_view kCompilerVersion = "1.6.0";
-inline constexpr std::string_view kCompilerVersionTag = "v1.6.0";
+inline constexpr std::string_view kCompilerVersion = "1.8.0";
+inline constexpr std::string_view kCompilerVersionTag = "v1.8.0";
 
 }  // namespace thagc::support
